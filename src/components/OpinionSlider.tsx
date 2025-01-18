ba84310fdc38e3d98b9400e10692bd7d5801d10a
Dawid Slowik <dawidslowik379@gmail.com>
2025-01-18 19:49:54 +0100

    Fix build error

--- src/components/OpinionSlider.tsx
+++ src/components/OpinionSlider.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
