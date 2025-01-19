ceaa455f16ab3b78a5b9fc9ef6447c941405a476
Dawid Slowik <dawidslowik379@gmail.com>
2025-01-19 11:58:17 +0100

    fix bug

--- src/components/ImagesSlider.tsx
+++ src/components/ImagesSlider.tsx
@@ -1,36 +1,9 @@
 "use client";
-import React from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
-import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 
 const ImagesSlider = () => {
-  const settings = {
-    infinite: true,
-    speed: 1000,
-    autoplaySpeed: 10000,
-    slidesToShow: 1,
-    autoplay: true,
-    slidesToScroll: 1,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 720,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Slider {...settings} className="">
-        <Image />
-      </Slider>
-    </div>
-  );
+  return <div>img slider</div>;
 };
 
 export default ImagesSlider;
